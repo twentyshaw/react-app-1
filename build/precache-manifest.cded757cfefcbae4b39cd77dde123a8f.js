35dbfad253e68908ae766e753a3391e0aafaa793
twentyshaw <isherlock951106@gmail.com>
2019-08-14 17:54:45 +0900

    build again

---
+++ build/precache-manifest.cded757cfefcbae4b39cd77dde123a8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf593cc870fbb5a72f256e0664642ca4",
+    "url": "/react-app-1/build/index.html"
+  },
+  {
+    "revision": "8cf150335114a0f7acf1",
+    "url": "/react-app-1/build/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "6dd914cc91aaf67d8ae9",
+    "url": "/react-app-1/build/static/js/2.1fc4f52d.chunk.js"
+  },
+  {
+    "revision": "8cf150335114a0f7acf1",
+    "url": "/react-app-1/build/static/js/main.cf504864.chunk.js"
+  },
+  {
+    "revision": "9f9ef90c78f5bea268ef",
+    "url": "/react-app-1/build/static/js/runtime~main.6b089628.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app-1/build/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.858e076b7f2d3b6f1f63392f35fa2ef3.js"
+  "/react-app-1/build/precache-manifest.cded757cfefcbae4b39cd77dde123a8f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-app-1/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
